24d1e6dfe33c4e1bc00ecc7aa86e57e93ce7310a
Ian Fragoso <IanPaul@Ians-MacBook-Pro.local>
2019-11-08 21:07:14 -0700

    finished cards index.js , finished with  MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,51 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+
+
+
+  function createCard(info) {
+        const 
+        card = document.createElement('div');
+        articleHeadline = document.createElement('div');
+        author = document.createElement('div');
+        imgContainer = document.createElement('div');
+        imgUrl = document.createElement('img');
+        authorName = document.createElement('span');
+
+        card.appendChild(articleHeadline);
+        card.appendChild(author);
+        author.appendChild(imgContainer);
+        imgContainer.appendChild(imgUrl);
+        author.appendChild(authorName);
+
+        imgUrl.src = info.authorPhoto;
+
+        card.classList.add('card');
+        articleHeadline.classList.add('headline');
+        author.classList.add('author');
+        imgContainer.classList.add('img-container');
+
+        articleHeadline.textContent = info.headline;
+        authorName.textContent = "By " + info.authorName;
+
+        return card;
+ } 
+ 
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+     .then((response) => {
+         let myCard = document.querySelector('.cards-container');
+         let arrayStuff = Object.values(response.data.articles);
+         arrayStuff.forEach(element => {
+             element.forEach(article => {
+                 myCard.appendChild(createCard(article));
+                 console.log('Created Card');
+             })
+         })
+     })
+     .catch((error) => {
+         console.log("data not returned");
+         console.log(error);
+     })
